f946a3449169f7ec4ba041f1e66d7b572182eddd
akvs <akarenvs@outlook.com>
2024-08-08 14:06:36 -0600

    feat(cart): add cart functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,96 @@
 import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Guitar from "./components/Guitar";
+import { db } from "./data/db";
 
 export default function App() {
 
-  const [auth, setAuth] = useState(false)
+  const initialCart = () => {
+    const localStorageCart = localStorage.getItem('cart')
+    return localStorageCart ? JSON.parse(localStorageCart) : []
+  }
 
+  const [data, setData] = useState(db);
+  const [cart, setCart] = useState(initialCart);
 
-  useEffect (() => {
-    console.log('componente lsito')
-    
-  }, [auth])
+  const MAX_ITEMS = 5
+  const MIN_ITEMS = 1
+
+  useEffect(() => { //cada que cambie se ejecutara este useEffect para solucionar tema de asincronia del state
+    localStorage.setItem('cart', JSON.stringify(cart))
+  },[cart])
+
+  function addToCart(item) {
+    const itemExists = cart.findIndex((guitar) => guitar.id === item.id);
+    if(item.quantity >= MAX_ITEMS) return
+    if (itemExists >= 0) {
+      const newCart = [...cart];
+      newCart[itemExists].quantity++;
+      setCart(newCart);
+    } else {
+      item.quantity = 1;
+      setCart([...cart, item]);
+    }
+  }
+
+  function removeFromCart(id) {
+    setCart(prevCart => prevCart.filter(guitar  => guitar.id !== id))
+  }
+
+  function increaseQuantity(id) {
+    const updateCart = cart.map(item => {
+      if(item.id === id && item.quantity < MAX_ITEMS){
+        return{
+          ...item,
+          quantity: item.quantity + 1
+        }
+      }
+      return item
+    })
+    setCart(updateCart)
+  }
+
+  function decreaseQuantity(id) {
+    console.log('decrementar');
+    const updateCart = cart.map(item => {
+      if(item.id === id && item.quantity > MIN_ITEMS) {
+        return{
+          ...item,
+          quantity: item.quantity - 1
+        }
+      }
+      return item
+    })
+    setCart(updateCart)
+  }
+
+  function clearCart () {
+    setCart([]);
+  }
+ 
 
   return (
     <>
-      <Header />
+      <Header
+        removeFromCart={removeFromCart}
+        increaseQuantity={increaseQuantity}
+        decreaseQuantity= {decreaseQuantity}
+        clearCart = {clearCart}
+        cart={cart}
+      />
       <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
-         <Guitar/>
+          {data.map((guitar) => (
+            <Guitar
+              key={guitar.id}
+              guitar={guitar}
+              setCart={setCart}
+              addToCart={addToCart}
+              auth={true}
+            />
+          ))}
         </div>
       </main>
 
